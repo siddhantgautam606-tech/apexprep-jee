2b63a2d5b0b3050bb2fed125012d45ad1243514c
siddhantgautam606-tech <siddhantgautam606@gmail.com>
2026-09-25 22:56:54 +0530

    Bump PrepXAI app version to 1.0.3

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.0.2';
+export const APP_VERSION = '1.0.3';
 export const APP_VERSION_URL = 'https://www.prepxai.co.in/app-version.json';
-export const APP_UPDATE_URL = 'https://www.prepxai.co.in/PrepXAI.apk?v=1.0.2';
+export const APP_UPDATE_URL = 'https://www.prepxai.co.in/PrepXAI.apk?v=1.0.3';
