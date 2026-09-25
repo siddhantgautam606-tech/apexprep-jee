774c937b4dcb95d3080152ddad9ea7aa3c177124
siddhantgautam606-tech <siddhantgautam606@gmail.com>
2026-09-25 11:51:22 +0530

    Add PrepXAI Android version and update configuration

---
+++ src/config/appVersion.js
@@ -0,0 +1,3 @@
+export const APP_VERSION = '1.0.1';
+export const APP_VERSION_URL = 'https://www.prepxai.co.in/app-version.json';
+export const APP_UPDATE_URL = 'https://cdn.jsdelivr.net/gh/siddhantgautam606-tech/apexprep-jee@main/public/PrepXAI.apk';
